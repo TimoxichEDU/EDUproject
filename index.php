3e7cc42b374a29cece4259bbf70cbfd014645c26
TimoxichEDU <147083331+TimoxichEDU@users.noreply.github.com>
2023-10-11 17:44:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laba 1</title>
+    </head>
+    <body>
+        <div class="mydiv1">
+            <?php
+                echo "laba 222";
+                echo 2+2;
+            ?>
+        </div>
+    </body>
+</html>
